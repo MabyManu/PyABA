bb4609376ab44fbc620ee36fbf023ab36e614534
DESKTOP-385H4M1\manum <manu.maby@inserm.fr>
2023-07-17 18:44:48 +0200

    Indent format correction

--- mne_tools.py
+++ mne_tools.py
@@ -124,50 +124,44 @@ def PlotEvokedDeviationFrom0(X,evokeds,colors_config,styles_config,alpha):
 
     
 def SpatTemp_TFCE_plotCompare(X, colors_config, evokeds,p_accept,n_permutations):
-    samplingFreq = evokeds[list(evokeds.keys())[0]].info['sfreq']
-    
-    
-    adjacency, ch_names = find_ch_adjacency(evokeds[list(evokeds.keys())[0]].info, ch_type='eeg')
-        
-    # Extract data: transpose because the cluster test requires channels to be last
-    # In this case, inference is done over items. In the same manner, we could
-    # also conduct the test over, e.g., subjects.
-
-    tfce = dict(start=1, step=2)
-    
-    # Calculate statistical thresholds
-    t_obs, clusters, cluster_pv, h0 = spatio_temporal_cluster_test(
-        X, tfce, adjacency=adjacency,
-        n_permutations=1000)  # a more standard number would be 1000+
-    significant_points = cluster_pv.reshape(t_obs.shape).T <= p_accept
-    print(str(significant_points.sum()) + " points selected by TFCE ...")
-    
-    figtopocompare = plot_compare_evokeds(evokeds, picks='eeg', colors=colors_config,axes='topo',split_legend=False,legend="lower center")
-    Nchans = t_obs.shape[1]
-
-    for i_chan in range (Nchans):
-        SignifiWin_curr = significant_points[i_chan,:]
-        SampSign =np.where(SignifiWin_curr)[0]
-        BoundWin_ix = np.where((np.diff(SampSign)>1))[0]
-        
-        if (len(BoundWin_ix)>0):
-            Nb_Clust = len(BoundWin_ix)+1
-            Clust_start = np.zeros(Nb_Clust,dtype='int64')
-            Clust_stop = np.zeros(Nb_Clust,dtype='int64')
-            
-            for i_clustwin in range(Nb_Clust):
-                if (i_clustwin==0):
-                    Clust_start[i_clustwin] = SampSign[0]
-                else:
-                    Clust_start[i_clustwin] = SampSign[BoundWin_ix[i_clustwin-1]+1]
-                
-                if (i_clustwin==(Nb_Clust-1)):
-                    Clust_stop[i_clustwin] = SampSign[-1]
-                else:
-                    Clust_stop[i_clustwin] = SampSign[BoundWin_ix[i_clustwin]]
-                figtopocompare[0].get_axes()[i_chan].axvspan(Clust_start[i_clustwin]/samplingFreq,Clust_stop[i_clustwin]/samplingFreq,facecolor="crimson",alpha=0.3)
-    figtopocompare[0].set_size_inches(8, 8)
+	samplingFreq = evokeds[list(evokeds.keys())[0]].info['sfreq']
+	adjacency, ch_names = find_ch_adjacency(evokeds[list(evokeds.keys())[0]].info, ch_type='eeg')
 	
+	# Extract data: transpose because the cluster test requires channels to be last
+	# In this case, inference is done over items. In the same manner, we could
+	# also conduct the test over, e.g., subjects.
+	
+	tfce = dict(start=1, step=2)
+	
+	# Calculate statistical thresholds
+	t_obs, clusters, cluster_pv, h0 = spatio_temporal_cluster_test(X, tfce, adjacency=adjacency,n_permutations=1000)  # a more standard number would be 1000+
+	significant_points = cluster_pv.reshape(t_obs.shape).T <= p_accept
+	print(str(significant_points.sum()) + " points selected by TFCE ...")
+    
+	figtopocompare = plot_compare_evokeds(evokeds, picks='eeg', colors=colors_config,axes='topo',split_legend=False,legend="lower center")
+	Nchans = t_obs.shape[1]
+	
+	for i_chan in range (Nchans):
+		SignifiWin_curr = significant_points[i_chan,:]
+		SampSign =np.where(SignifiWin_curr)[0]
+		BoundWin_ix = np.where((np.diff(SampSign)>1))[0]
+		
+		if (len(BoundWin_ix)>0):
+			Nb_Clust = len(BoundWin_ix)+1
+			Clust_start = np.zeros(Nb_Clust,dtype='int64')
+			Clust_stop = np.zeros(Nb_Clust,dtype='int64')
+			for i_clustwin in range(Nb_Clust):
+				if (i_clustwin==0):
+					Clust_start[i_clustwin] = SampSign[0]
+				else:
+					Clust_start[i_clustwin] = SampSign[BoundWin_ix[i_clustwin-1]+1]
+				
+				if (i_clustwin==(Nb_Clust-1)):
+					Clust_stop[i_clustwin] = SampSign[-1]
+				else:
+					Clust_stop[i_clustwin] = SampSign[BoundWin_ix[i_clustwin]]
+				figtopocompare[0].get_axes()[i_chan].axvspan(Clust_start[i_clustwin]/samplingFreq,Clust_stop[i_clustwin]/samplingFreq,facecolor="crimson",alpha=0.3)
+	figtopocompare[0].set_size_inches(8, 8)
 	return figtopocompare
     
     # Emergence_clust_bool = np.zeros((evokeds.times.size,evokeds.info['nchan']),dtype='bool')
